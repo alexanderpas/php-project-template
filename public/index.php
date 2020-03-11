fa2b6a6c0bce689c798a6e3bb1e73aa8a24afe6b
Alexander Pas <git@dropdev.org>
2020-03-12 00:21:00 +0100

    Initial version

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Point of entry for all web requests.
+ */
+
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
---
+++ run.php
@@ -0,0 +1,10 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ * Point of entry for all console commands.
+ */
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/autoload.php';
